89da7318eb2477289ea43b6e417abacd43ae2912
prof79 <markus@markusegger.at>
2018-08-15 01:37:41 +0200

    Added new XAML-based main page.

---
+++ Exercise 1/Start/Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿// MainPage.xaml.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
