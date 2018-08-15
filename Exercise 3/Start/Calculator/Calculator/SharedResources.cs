1b4de0321cda30dc33a2c879d92be875b30da342
prof79 <markus@markusegger.at>
2018-08-15 12:20:26 +0200

    All colors to constants.

--- Exercise 3/Start/Calculator/Calculator/SharedResources.cs
+++ Exercise 3/Start/Calculator/Calculator/SharedResources.cs
@@ -9,7 +9,31 @@ namespace Calculator
 {
     public static class SharedResources
     {
+        public static Color CanvasBkColor
+            => Color.Black;
+
+        public static Color DisplayLabelBkColor
+            => Color.Black;
+
+        public static Color DisplayLabelForeColor
+            => Color.White;
+
+        public static Color NumberButtonBkColor
+            => Color.White;
+
+        public static Color NumberButtonForeColor
+            => Color.Black;
+
         public static Color OpButtonBkColor
             => Color.FromRgb(0xff, 0xa5, 0x00);
+
+        public static Color OpButtonForeColor
+            => Color.White;
+
+        public static Color ClearButtonBkColor
+            => Color.FromRgb(0x80, 0x80, 0x80);
+
+        public static Color ClearButtonForeColor
+            => Color.White;
     }
 }
