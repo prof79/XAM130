c6572fbb91197334c29ab69494e7afc49f088196
prof79 <markus@markusegger.at>
2018-08-15 12:03:26 +0200

    Styled operator buttons using static constants.

---
+++ Exercise 3/Start/Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,15 @@
+﻿// SharedResources.cs
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+            => Color.FromRgb(0xff, 0xa5, 0x00);
+    }
+}
